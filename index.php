083f8ecaedcf42ac0aa8f26df3418d768206f139
Anne <racera@luntiands.com>
2026-03-11 21:27:19 +0800

    icon

--- index.php
+++ index.php
@@ -49,6 +49,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>Login - Luntian</title>
+    <link rel="icon" type="image/png" href="assets/img/luntian-favicon.png">
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
